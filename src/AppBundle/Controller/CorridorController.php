245a0cfde459a37f9c3e91046dd05d2553b7779d
babacarbasse <babacar.basse@esp.sn>
2018-01-20 02:26:03 +0000

    creations des controller

---
+++ src/AppBundle/Controller/CorridorController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: babacarbasse
+ * Date: 20/01/18
+ * Time: 02:24
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CorridorController extends Controller
+{
+
+}
---
+++ src/AppBundle/Controller/RoomController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: babacarbasse
+ * Date: 20/01/18
+ * Time: 02:25
+ */
+
+namespace AppBundle\Controller;
+
+
+class RoomController
+{
+
+}
